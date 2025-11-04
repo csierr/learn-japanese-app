10f060c776be1fdeca1f934fe9c6ca9213155416
csierr <catalina.sierra@uc.cl>
2025-11-04 11:51:53 -0300

    chore: implement translation endpoints with romaji

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
+import os
+from unittest import result
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from pykakasi import kakasi
+import deepl
+from dotenv import load_dotenv
+load_dotenv()
 
-app = FastAPI()
-
-# Configure CORS to allow requests from your frontend
-# Using ["*"] is convenient for development.
-# For production, you should restrict this to your frontend's domain.
-origins = ["*"]
+auth_key = os.getenv("DEEPL_API_KEY")
+app      = FastAPI()
+origins  = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,15 +20,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+deepl_client = deepl.DeepLClient(auth_key)
+
+k = kakasi()
+k.setMode("H", "a") # Hiragana to Ascii (Romaji)
+k.setMode("K", "a") # Katakana to Ascii (Romaji)
+k.setMode("J", "a") # Kanji to Ascii (Romaji)
+conv = k.getConverter()
+
 class TranslationRequest(BaseModel):
     text: str
 
 @app.post("/api/translate")
 async def translate_text(request: TranslationRequest):
-    # TODO: Implement actual translation logic here
-    # For now, we'll return a mock translation.
-    print(f"Received text to translate: {request.text}")
-    return {"translatedText": f"'{request.text}' translated to Japanese."}
+    result = deepl_client.translate_text(request.text, target_lang="JA")
+    romaji_text = conv.do(result.text)
+    return {"translatedText": f"{result.text}", "romajiText": romaji_text}
+
+@app.post("/api/name_translate")
+async def translate_name(request: TranslationRequest):
+    result = deepl_client.translate_text(request.text, target_lang="JA")
+    romaji_name = conv.do(result.text)
+    return {"translatedName": f"{result.text}", "romajiName": romaji_name}
 
 @app.get("/")
 def read_root():
